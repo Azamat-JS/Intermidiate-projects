6c83771eae4ead90a7c7e09ea0c0cf42d1706894
Azamat-JS <azamatielts987@gmail.com>
2025-02-08 14:42:24 +0500

    profile section was added to Library project

--- Library project/controllers/profil.js
+++ Library project/controllers/profil.js
@@ -2,22 +2,30 @@ const Auth = require('../models/auth')
 const BaseError = require('../errors/base_error')
 
 const getProfile = async(req, res, next) => {
-    const {id} = req.params
+    const id = req.user.userId
     const profile = await Auth.findById(id)
     if(!profile){
-       return next(BaseError.NotFoundError(`There is no profil with id: ${id}`))
+       return next(BaseError.NotFoundError(`You don't have an account please register or login`))
     }
     res.status(200).json(profile)
 }
 
 const updateProfile = async(req, res, next) => {
-    const user_info = req.user.userId
-    const profile = await Auth.findByIdAndUpdate(user_info, req.body, 
+   if(!req.user || !req.user.userId){
+    return next(BaseError.BadRequestError('You should register or login before updating the profil'))
+   }
+   const userId = req.user.userId
+   const updateData = {...req.body};   
+   if(req.fileUrl){
+    updateData.image = req.fileUrl
+   }
+
+    const profile = await Auth.findByIdAndUpdate(userId, updateData, 
         {new: true, runValidators: true})
         if(!profile){
-           return next(BaseError.NotFoundError(`There is no user with this id: ${user_info}`))
+           return next(BaseError.NotFoundError(`No user found with id: ${userId}`))
         }
-        res.status(200).json(profile)
+        res.status(200).json({success: true, data: profile})
 }
 
 const deleteProfile = async(req, res) => {
--- Library project/routes/profile_rt.js
+++ Library project/routes/profile_rt.js
@@ -1,9 +1,10 @@
 const router = require('express').Router()
 const {getProfile, updateProfile, deleteProfile} = require('../controllers/profil')
 const { tokenChecker } = require('../middleware/checkToken')
+const ImageUpload = require('../utils/multer')
 
-router.get('/profile/:id', tokenChecker, getProfile)
-router.put('/updateProfile', tokenChecker, updateProfile)
+router.get('/profile', tokenChecker, getProfile)
+router.put('/updateProfile', tokenChecker, ImageUpload.singleImage, updateProfile)
 router.delete('/deleteProfile', tokenChecker, deleteProfile)
 
 module.exports = router
