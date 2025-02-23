fb4777b2a25abd51c575a1f5e0e3cee9248b4b24
Azamat-JS <azamatielts987@gmail.com>
2025-02-23 19:50:27 +0500

    added data

---
+++ Graph/server/sampleData.js
@@ -0,0 +1,91 @@
+const projects = [
+    {
+        id: '1',
+        clientId: '1',
+        name: 'eCommerce Website',
+        description: 
+        'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis necessitatibus beatae quaerat, reprehenderit quod at ullam, optio voluptates, autem saepe in dolores obcaecati. Est incidunt dolores quia nam sapiente ab.',
+        status: 'In process'
+    },
+    {
+        id: '2',
+        clientId: '2',
+        name: 'Tech Website',
+        description: 
+        'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis necessitatibus beatae quaerat, reprehenderit quod at ullam, optio voluptates, autem saepe in dolores obcaecati. Est incidunt dolores quia nam sapiente ab.',
+        status: 'In process'
+    },
+    {
+        id: '3',
+        clientId: '3',
+        name: 'Employee Website',
+        description: 
+        'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis necessitatibus beatae quaerat, reprehenderit quod at ullam, optio voluptates, autem saepe in dolores obcaecati. Est incidunt dolores quia nam sapiente ab.',
+        status: 'In process'
+    },
+    {
+        id: '4',
+        clientId: '4',
+        name: 'Education Website',
+        description: 
+        'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis necessitatibus beatae quaerat, reprehenderit quod at ullam, optio voluptates, autem saepe in dolores obcaecati. Est incidunt dolores quia nam sapiente ab.',
+        status: 'In process'
+    },
+    {
+        id: '5',
+        clientId: '5',
+        name: 'Healthcare Website',
+        description: 
+        'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis necessitatibus beatae quaerat, reprehenderit quod at ullam, optio voluptates, autem saepe in dolores obcaecati. Est incidunt dolores quia nam sapiente ab.',
+        status: 'In process'
+    },
+    {
+        id: '6',
+        clientId: '6',
+        name: 'Politics Website',
+        description: 
+        'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis necessitatibus beatae quaerat, reprehenderit quod at ullam, optio voluptates, autem saepe in dolores obcaecati. Est incidunt dolores quia nam sapiente ab.',
+        status: 'In process'
+    },
+]
+
+const clients =  [
+    {
+        id: '1',
+        name: 'Tony Shark',
+        email: 'iron@gmail.com',
+        phone: '91 000 99 99'
+    },
+    {
+        id: '2',
+        name: 'John Dow',
+        email: 'ilon@gmail.com',
+        phone: '91 333 99 99'
+    },
+    {
+        id: '3',
+        name: 'Tony Dep',
+        email: 'dep@gmail.com',
+        phone: '91 000 98 99'
+    },
+    {
+        id: '4',
+        name: 'Kevin Shark',
+        email: 'fer@gmail.com',
+        phone: '91 897 99 99'
+    },
+    {
+        id: '5',
+        name: 'Tony Yen',
+        email: 'tony@gmail.com',
+        phone: '91 000 12 99'
+    },
+    {
+        id: '6',
+        name: 'Ben Shark',
+        email: 'ben@gmail.com',
+        phone: '99 100 99 99'
+    },
+]
+
+module.exports = {projects, clients}
