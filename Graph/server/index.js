de78c050f3e28421be7175c765e532fd2116c176
Azamat-JS <azamatielts987@gmail.com>
2025-02-23 22:39:03 +0500

    added MongoDB to eCommerce project with graphQl

--- Graph/server/index.js
+++ Graph/server/index.js
@@ -1,9 +1,19 @@
 require('dotenv').config({path: __dirname + '/.env'})
+const colors = require('colors')
 const express = require('express')
 const path = require('path')
 const {graphqlHTTP} = require('express-graphql')
 const schema = require('./schema/schema')
+const cors = require('cors')
+const connectDB = require('./db/connect')
+
+
 const app = express()
+
+app.use(express.json())
+app.use(cors({credentials:true, origin:"*"}))
+
+connectDB()
 
 app.use('/graphql', graphqlHTTP({
 schema,
@@ -13,7 +23,7 @@ graphiql:process.env.NODE_ENV === 'development'
 
 const PORT = process.env.PORT || 4000
 app.listen(PORT, () => {
-    console.log('server is running on port: ' + PORT);
+    console.log(`server is running on port http://localhost:${PORT}/graphql`);
 })
 
 
