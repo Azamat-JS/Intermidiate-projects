64d74e6dcd35f50b367e8ec2e7e088fd1296be3f
Azamat-JS <azamatielts987@gmail.com>
2025-02-26 10:49:49 +0500

    pagination and project details implemented into Graph project

--- Graph/client/src/App.js
+++ Graph/client/src/App.js
@@ -1,7 +1,9 @@
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 import Header from '../src/components/Header'
-import {ApolloProvider, ApolloClient, InMemoryCache} from '@apollo/client'
-import Client from '../src/components/Client'
-import AddClientModal from './components/AddClientModal'
+import {ApolloProvider, ApolloClient, InMemoryCache, from} from '@apollo/client'
+import Home from './pages/Home'
+import NotFound from './pages/NotFound'
+import Project from './pages/Project'
 
 const cache = new InMemoryCache({
   typePolicies: {
@@ -32,11 +34,16 @@ function App() {
   return (
     <>
     <ApolloProvider client={client}>
+      <Router>
     <Header/>
     <div className="container">
-      <AddClientModal/>
-    <Client/>
+   <Routes>
+    <Route path='/' element={<Home/>}/>
+    <Route path='/projects/:id' element={<Project/>}/>
+    <Route path='*' element={<NotFound/>}/>
+   </Routes>
     </div>
+        </Router>
     </ApolloProvider>
     </>
   );
---
+++ Graph/client/src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import Client from '../components/Client'
+import AddClientModal from '../components/AddClientModal'
+import Projects from '../components/Projects'
+
+export default function Home() {
+  return (
+    <>
+    <div className="d-flex gap-3 mb-4">
+     <AddClientModal/>
+    </div>
+      <Projects/>
+      <hr />
+    <Client/>
+    </>
+  )
+}
