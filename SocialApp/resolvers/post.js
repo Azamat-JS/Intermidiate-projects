7af0ee4fed921fdc0e6722d3fd3a83d334c5ac52
Azamat-JS <azamatielts987@gmail.com>
2025-03-03 17:51:23 +0500

    like posts implemented in social app

--- SocialApp/resolvers/post.js
+++ SocialApp/resolvers/post.js
@@ -52,6 +52,26 @@ const {AuthenticationError}= require('apollo-server')
           } catch (error) {
             throw new Error(error)
           }
+       },
+
+       async likePost(_, {postId}, context){
+        const {username} = checkAuth(context);
+        const post = await Post.findById(postId);
+
+        if(post){
+          if(post.likes.find(like => like.username === username)){
+            // Post already like, unlike it
+            post.likes = post.likes.filter((like => like.username !== username))
+          }else{
+            // not liked, like post
+           post.likes.push({
+            username,
+            createdAt: new Date().toISOString()
+           }) 
+          }
+          await post.save()
+          return post;
+        }
        }
     }
 }
